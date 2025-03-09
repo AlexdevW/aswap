d60e74078f95191bd1627413142d5595fbb398d8
alex <alexdevw@gmail.com>
2025-03-09 22:10:26 +0800

    fix: 样式调整

--- apps/web/src/app/pool/page.tsx
+++ apps/web/src/app/pool/page.tsx
@@ -3,7 +3,7 @@ import PoolTable from "./_components/pool-table"
 
 export default function Pool() {
   return (
-    <div className="container py-10 bg-white rounded-3xl w-full">
+    <div className="container py-10 bg-white rounded-3xl w-full max-w-full">
       <div className="text-2xl font-bold">资金池</div>
       <PoolTable />
     </div>
--- apps/web/src/app/positions/page.tsx
+++ apps/web/src/app/positions/page.tsx
@@ -3,7 +3,7 @@ import PositionsTable from "./_components/positions-table"
 
 export default function Positions() {
   return (
-    <div className="container py-10 bg-white rounded-3xl w-full">
+    <div className="container py-10 bg-white rounded-3xl w-full max-w-full">
       <div className="text-2xl font-bold">头寸</div>
       <PositionsTable />
     </div>
